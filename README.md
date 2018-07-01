08d3c7dd4f8cb037b3b87dfb8273dad52dbbce22
Donagh <xiddler@gmail.com>
2018-07-01 16:51:21 +0100

    initial commint 2018-07-01_16:51

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # lanner
 mobile sends snippets to laptop and vice versa
+I added this in the terminal before pushing to remote
---
+++ create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE snippets (
+    id integer PRIMARY KEY NOT NULL,
+    snippet text NOT NULL
+);
+
+INSERT INTO  snippets (snippet) values ('erjlklko45djdgkl /gdjgdfkflk');
+
