47d627eec5d49893871c927c08f095044571fa72
Donagh <xiddler@gmail.com>
2020-12-30 09:48:28 +0000

    updated on laptop; prior to deploying on RPi

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # lanner
-mobile sends snippets to laptop and vice versa
+
+2020-03-14_13:11
+This is almost 2 years later and the script is still working. The styling / css is still baaad. 
+
+
+sends snippets to laptop from smartphone and vice versa
 i added this 2018-07-02_10:11 
 I added this in the terminal before pushing to remote
+
+added .flaskenv and .env to utilise Flask 1.0 $flask run mode
+
+
---
+++ run.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, url_for, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	# INSERT new snippet
+	data = request.form
+	snippet = data.get('snippet', 'no message')
+	conn = sqlite3.connect('snippets.db')
+	# print("Database opened successfully")
+	cur = conn.cursor()
+	cur.execute("INSERT INTO snippets(snippet) values (?)", (snippet, ))
+	# opt = cur.fetchall()
+	# opt = cur.fetchone()
+	conn.commit() 
+	conn.close() 
+	#	
+	# OUTPUT new snippet
+	conn = sqlite3.connect('snippets.db')
+	# print("Opened database successfully")
+	cur = conn.cursor()
+	cur.execute("SELECT snippet FROM snippets ORDER BY id DESC; ")
+	all_snippets = cur.fetchall()
+	# fetchall() returns a row list
+	# test = cur.fetchone()
+	# most_recent = [item[0] for item in all_snippets[0:2]]
+	most_recent1 = all_snippets[0:1][0][0]
+	most_recent2 = all_snippets[1:2][0][0]
+	conn.close()	
+	# most_recent1 = snippet
+	# most_recent2 = "this2"
+	return render_template('index.html', most_recent1=most_recent1, most_recent2=most_recent2)
+
+if __name__ == '__main__':
+	app.run(host ='0.0.0.0', port=9090, debug=True) # '0.0.0.0' allows browsing from other devices on the lan.
+
